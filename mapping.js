d642e972454e69dd2e7d048f160de9044a3d35ef
rcackerman <rebecca@cfa>
2014-02-05 17:47:38 -0800

    Adding attribution and testing git settings.

--- mapping.js
+++ mapping.js
@@ -1,8 +1,9 @@
 // create map
 
 var map = L.map('map').setView([42.3540, -83.0523], 11);
-L.tileLayer('http://api.tiles.mapbox.com/v3/rcackerman.h6ofgio1/{z}/{x}/{y}.png'
-            ).addTo(map);
+L.tileLayer('http://api.tiles.mapbox.com/v3/rcackerman.h6ofgio1/{z}/{x}/{y}.png', {
+            attribution: 'Made pretty by Mapbox'
+          }).addTo(map);
 
 // layers to be added
 
