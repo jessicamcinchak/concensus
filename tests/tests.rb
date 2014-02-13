776bcd65749836a45f676fdfe8b6418960df87fd
rcackerman <rebecca@cfa>
2014-02-12 16:35:06 -0800

    Adding basic tests.

--- tests/tests.rb
+++ tests/tests.rb
@@ -1,4 +1,25 @@
-require 'ConCensus'
-require 'test/unit'
+require './app'
+require 'minitest/autorun'
 require 'rack/test'
 
+ENV['RACK_ENV'] = 'test'
+
+class HelloWorldTest < MiniTest::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    ConCensus
+  end
+
+  def test_can_reach_index
+    get '/'
+    assert last_response.ok?
+    assert_equal "Hello, World!", last_response.body
+  end
+
+  def test_can_reach_map
+  	get '/map'
+  	assert last_response.ok?
+  	assert_equal "Hello, World!", last_response.body
+  end
+end
