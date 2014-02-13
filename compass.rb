a8953dc5474d64a0230f8499abd21e8c2a673592
rcackerman <rebecca@cfa>
2014-02-12 16:32:39 -0800

    Compiling CSS.

---
+++ compass.rb
@@ -0,0 +1,34 @@
+# This is the config file for Compass
+
+require './app'
+
+# Require any additional compass plugins here.
+require "bootstrap-sass"
+
+project_path 							= Sinatra::Application.root
+
+# Set this to the root of your project when deployed:
+http_path 								= "/assets"
+http_stylesheets_path 		= "/assets/stylesheets"
+css_dir 									= File.join 'assets', "stylesheets"
+sass_dir 									= File.join 'assets', 'sass'
+images_dir			 					= File.join 'assets', "images"
+javascripts_dir 					= File.join 'assets', "scripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+output_style 							= :extended
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets 					= true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
