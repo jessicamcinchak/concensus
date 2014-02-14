71794939fe9b899a54a46c897a9be71d6493662e
rcackerman <rebecca@cfa>
2014-02-13 16:43:41 -0800

    Template helper didn't work with bootstrap.

--- helpers/render_partial.rb
+++ helpers/render_partial.rb
@@ -1,7 +1,7 @@
-module RenderPartial
-  def partial(page, options={})
-    haml page, options.merge!(:layout => false)
-  end
-end
+# module RenderPartial
+#   def partial(page, options={})
+#     haml page, options.merge!(:layout => false)
+#   end
+# end
 
-helpers RenderPartial
+# helpers RenderPartial
