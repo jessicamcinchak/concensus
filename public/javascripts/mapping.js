c8ca4e7e80e3dc6e6b3135543222873716496504
rcackerman <rebecca@cfa>
2014-02-22 12:14:02 -0800

    Updating map page to use jquery to pull in jon files. Map data is messed up, though, so it's not working with leaflet.

--- public/javascripts/mapping.js
+++ public/javascripts/mapping.js
@@ -1,4 +1,51 @@
-// create map
+// Layer groups for base layers and point layers
+var bases = new L.LayerGroup(),
+		points = new L.LayerGroup();
+
+function style(feature) {
+  return {
+    fillColor: '#BF6FA0',
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7
+  };
+}
+// Data sources
+
+// Right now the data sources are hardcoded, since there are only 4
+// TODO: - add data sources dynamically as users select layers
+console.log("Starting data...");
+
+function getShapeData( geoURL ) {
+	$.getJSON( geoURL , function(data) {
+		  var geolayer = L.geoJson(data, {
+		  	style: style
+		  });
+		  geolayer.addTo(map);
+		  console.log(geolayer);
+	});
+}
+
+// var DetroitBlockGroup = getShapeData("/data/blockgroups.json");
+// 		DetroitTract = omnivore.csv('data/DetroitTract2010.csv'),
+// 		streetlights = L.geoJson(''),
+// 		demographics = L.geoJson('');
+
+// These are the type of layer groups the control panel will show
+
+// var baseLayers = {
+// 						"Census Block Groups": DetroitBlockGroup,
+// 						// "Census Tracts": DetroitTract
+// };
+
+// var pointLayers = {
+// 						"Street Lights": streetlights,
+// 						"Demographics": demographics
+// };
+
+// // create map
 
 var map = L.map('map', {
 												center: [42.3540, -83.0523],
@@ -10,30 +57,6 @@ L.tileLayer('http://api.tiles.mapbox.com/v3/rcackerman.h6ofgio1/{z}/{x}/{y}.png'
             attribution: 'Made pretty by Mapbox'
           }).addTo(map);
 
-// Layer groups for base layers and point layers
-var bases = new L.LayerGroup(),
-		points = new L.LayerGroup();
-
-// Data sources
-// Right now the data sources are hardcoded, since there are only 4
-// TODO: - add data sources dynamically as users select layers
-var DetroitBlockGroup = L.geoJson('blockgroups.geojson'),
-		DetroitTract = L.geoJson(''),
-		streetlights = L.geoJson(''),
-		demographics = L.geoJson('');
-
-// These are the type of layer groups the control panel will show
-
-var baseLayers = {
-						"Census Block Groups": DetroitBlockGroup,
-						"Census Tracts": DetroitTract
-};
-
-var pointLayers = {
-						"Street Lights": streetlights,
-						"Demographics": demographics
-};
-
-
-// TODO: use the legend as the control, rather than this
-L.control.layers(baseLayers, pointLayers).addTo(map);
+getShapeData("/data/blockgroups.json");
+// // TODO: use the legend as the control, rather than this
+// L.control.layers(baseLayers, pointLayers).addTo(map);
