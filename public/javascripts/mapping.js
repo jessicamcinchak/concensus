1346f095a999e3158523933774396399791accfa
Talia Selitsky <talqsel@gmail.com>
2014-06-02 18:32:50 -0400

    accordion has proper radio buttons

--- public/javascripts/mapping.js
+++ public/javascripts/mapping.js
@@ -2,24 +2,21 @@
 // ---------
 
 var currentLayer;
+var currentData;
 var map = L.map('map', {
                         center: [42.3540, -83.0523],
                         zoom: 11,
                         layers: []
-});
+                        }
+                );
 
-L.tileLayer('http://api.tiles.mapbox.com/v3/rcackerman.h6ofgio1/{z}/{x}/{y}.png', {
+var baseMap=L.tileLayer('http://api.tiles.mapbox.com/v3/rcackerman.h6ofgio1/{z}/{x}/{y}.png', {
             attribution: 'Made pretty by Mapbox'
           }).addTo(map);
 
-//$("#accordion_d3" ).accordion();
 var accordion_d3=$(".accordion_d3" ).accordion();
-//.parent().find(".ui-accordion-header").css({'background-color':'#e4eef3'});
-console.log(accordion_d3);
 
 var accordion_user=$(".accordion_user" ).accordion();
-//.parent().find(".ui-accordion-header").css({'background-color':'#dde9c3'});
-console.log(accordion_user);
 
  $( "#dialog" ).dialog({
       autoOpen: false,
@@ -51,7 +48,6 @@ $( "#dialog1" ).dialog({
     });
 
     $('#dialog').click(function (evt) {
-      console.log("in begin");
       $( "#dialog1" ).dialog( "open" )
       .position({
        my: 'left',
@@ -61,11 +57,9 @@ $( "#dialog1" ).dialog({
     });
 
 
-
-
 function style( feature ) {
   return {
-    fillColor: '#EA929D',
+    fillColor: '#000000',
     weight: 2,
     opacity: 1,
     color: 'white',
@@ -74,9 +68,48 @@ function style( feature ) {
   };
 }
 
+var resetColor=function(newColor)
+{
+  var newStyle = {
+    "fillColor": newColor,
+    "weight": 2,
+    "opacity": 1,
+    "color": 'white',
+    "dashArray": '3',
+    "fillOpacity": 0.7
+  }
 
-// Data Getting
-// ---------
+var features=currentData['features'];
+
+  L.geoJson(features, {
+    style: newStyle
+  }).addTo(map);
+}
+
+ $(".rectangle1").click(function() {  
+   resetColor('#da5027');
+  });
+
+ $(".rectangle2").click(function() {  
+   resetColor('#f6a91c');
+  });
+
+ $(".rectangle3").click(function() {  
+   resetColor('#216a8b');
+  });
+
+  $(".rectangle4").click(function() {  
+   resetColor('#6596cf');
+  });
+
+   $(".rectangle5").click(function() {  
+   resetColor('#87af3e');
+  });
+
+   $(".rectangle6").click(function() { 
+   resetColor('#20592c');
+  });
+
 
 var processData = function( data, options ) {
   var geolayer = L.geoJson(data, {
@@ -85,14 +118,25 @@ var processData = function( data, options ) {
      layer.bindPopup(feature.properties[options.property]);
    }
   });
+
+  
   map.addLayer(geolayer, options.name);
+
+//Global Variables
+  currentData=data;
   currentLayer = geolayer;
 };
 
-var getData = function( geoURL, options ) {
+var getData = function( geoURL, options,fileName ) {
   var req = $.getJSON( geoURL );
+
   req.done( function(data) {
-    console.log(data);
+
+    if(fileName=='median_family.geojson')
+  {
+    var newFeatures=queryData(data, 'City', 'Detroit');
+    data['features']=newFeatures;
+  }
     processData(data, options);
   } );
 };
@@ -101,6 +145,22 @@ var removeData = function() {
   map.removeLayer(currentLayer);
 };
 
+var queryData=function(data, property, selectingProperty){
+  var features=data['features'];
+  var newFeatures=new Array();
+    for (var i = 0; i < features.length ;i++) {
+        var prop=features[i].properties[property]
+        if(prop==selectingProperty)
+        {
+          newFeatures.push(features[i]);
+        }
+    }
+
+    return newFeatures;
+};
+
+
+
 // Sidebar
 // ---------
 
@@ -108,11 +168,11 @@ $(".radio").click( function() {
 
  $( "#dialog" ).dialog( "open" );
 
-  var file = '/data/' + $(this).data("file");
+  var fileName=$(this).data("file");
+  var file = '/data/' + fileName;
   var datasetname = $(this).data("name");
   var infoOfImportance = $(this).data("property");
   var alreadyChecked = $(".checked");
-  console.log(alreadyChecked.find("input"));
   alreadyChecked.find("input").prop("checked", false);
   alreadyChecked.removeClass("checked");
  
@@ -123,5 +183,8 @@ $(".radio").click( function() {
   $(this).addClass("checked");
   $(this).find("input").prop("checked", true);
 
-  getData( file, {name: datasetname, property: infoOfImportance} );
+  getData( file, {name: datasetname, property: infoOfImportance},fileName );
 });
+
+
+
