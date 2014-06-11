a00dcfe091130ab22fac30e68eb954c724a7af18
Talia Selitsky <talqsel@gmail.com>
2014-06-11 13:13:24 -0400

    added new popup

---
+++ public/javascripts/colorGradients.js
@@ -0,0 +1,8 @@
+var input = { 
+    "pink": ["#F9E6E0","#F5D3C9","#ECA793", "#E37B5D", "#DA5027"],
+    "yellow": ['#FFF5E5','#FFE3B2','#FFD17F','#FFBF4C','#FFA400'],
+    "darkBlue": ['#DFE9EE','#C7D9E2','#90B4C5','#588FA8','#216A8B'],
+    "lightBlue": ['#EBF1F9','#D3E1F1','#B2CAE7','#8BB0DB','#6596CF'],
+    "lightGreen": ['#F0F5E6','#DCE8C7','#C3D79E','#A5C36E','#87AF3E'],
+    "darkGreen": ['#E3EAE4','#BFCFC2','#8FAC95','#578260','#20592C']
+}
